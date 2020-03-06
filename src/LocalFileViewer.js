5160d239a1c75d2a312cc282b26edf0203124c6f
Paper Lee <paper@diing.com>
2020-03-06 09:16:21 -0800

    Custom radio style hit performance

--- src/LocalFileViewer.js
+++ src/LocalFileViewer.js
@@ -7,7 +7,7 @@ const getEntityFileURL = (fileName, format) => {
     fileName = (fileName === undefined) ? "sample" : (fileName.length === 0) ? "sample" : fileName;
     const suffixReg = /\.(gltf|glb|usdz)$/gi;
     const gltfFileReg = /\.(gltf)$/gi;
-    const fn = fileName.replace(suffixReg, "");
+    const fn = ltrim(fileName.replace(suffixReg, ""));
     const isGltfFile = gltfFileReg.test(fileName) || format === "gltf";
     const prefix = `${process.env.PUBLIC_URL}/assets/entities`;
     return [`${prefix}/${fn}.${isGltfFile ? "gltf" : "glb"}`, `${prefix}/${fn}.usdz`, `${fn}.${isGltfFile ? "gltf" : "glb"}`];
@@ -22,13 +22,13 @@ const Form = (props) => {
                 <small id="fileNameHint" className="form-text text-muted">Add .gltf in suffix to specify a glTF file, eg. sample.gltf</small>
             </div>
             <div className="form-group">
-                <div className="custom-control custom-radio custom-control-inline">
-                    <input type="radio" value="glb" id="glb" name="fileFormat" className="custom-control-input" checked={props.format === "glb"} onChange={props.handleFormatOptionChange} />
-                    <label className="custom-control-label" htmlFor="glb">glb</label>
+                <div className="form-check form-check-inline">
+                    <input type="radio" value="glb" id="glb" name="fileFormat" className="form-check-input" checked={props.format === "glb"} onChange={props.handleFormatOptionChange} />
+                    <label className="form-check-label" htmlFor="glb">glb</label>
                 </div>
-                <div class="custom-control custom-radio custom-control-inline">
-                    <input type="radio" id="gltf" value="gltf" name="fileFormat" className="custom-control-input" checked={props.format === "gltf"} onChange={props.handleFormatOptionChange} />
-                    <label className="custom-control-label" htmlFor="gltf">glTF</label>
+                <div class="form-check form-check-inline">
+                    <input type="radio" id="gltf" value="gltf" name="fileFormat" className="form-check-input" checked={props.format === "gltf"} onChange={props.handleFormatOptionChange} />
+                    <label className="form-check-label" htmlFor="gltf">glTF</label>
                 </div>
             </div>
             <button type="submit" className="btn btn-warning clearfix" disabled={props.fileName === undefined || props.fileName.length < 1}>Load Entity</button>
@@ -40,13 +40,15 @@ const LocalFileViewer = () => {
     const modelViewerRef = useRef();
     const [fileName, setFileName] = useState();
     const [loadError, setLoadError] = useState(false);
-    const [submittedFileName, setSubmittedFileName] = useState();
     const [format, setFormat] = useState("glb");
-    const [submittedFormat, setSubmittedFormat] = useState(format);
+    const [submittedData, setSubmittedData] = useState({
+        fileName: "sample",
+        format: "glb"
+    });
 
     useEffect(() => {
         modelViewerRef.current.addEventListener('error', (evt) => {
-            console.log(`Loading model error: ${evt.detail.type} for ${getEntityFileURL(submittedFileName, submittedFormat)[2]}`);
+            console.log(`Loading model error: ${evt.detail.type} for ${getEntityFileURL(submittedData.fileName, submittedData.format)[2]}`);
             setLoadError(true);
         });
     });
@@ -54,8 +56,10 @@ const LocalFileViewer = () => {
     const handleSubmit = (evt) => {
         evt.preventDefault();
         setLoadError(false);
-        setSubmittedFileName(ltrim(fileName));
-        setSubmittedFormat(format);
+        setSubmittedData({
+            fileName: fileName,
+            format: format
+        });
     }
 
     const handleFormatOptionChange = (evt) => {
@@ -78,14 +82,14 @@ const LocalFileViewer = () => {
                 loadError && (
                     <div className="row">
                         <div className="col-12">
-                            <div className="alert alert-danger" role="alert">Failed to load the file {getEntityFileURL(submittedFileName, submittedFormat)[2]}</div>
+                            <div className="alert alert-danger" role="alert">Failed to load the file {getEntityFileURL(submittedData.fileName, submittedData.format)[2]}</div>
                         </div>
                     </div>
                 )
             }
             <div className="row">
                 <div className="model-viewer-box col-12">
-                    <model-viewer class="model-viewer" src={getEntityFileURL(submittedFileName, submittedFormat)[0]} ar auto-rotate camera-controls ios-src={getEntityFileURL(submittedFileName, submittedFormat)[1]} ref={modelViewerRef} />
+                    <model-viewer class="model-viewer" src={getEntityFileURL(submittedData.fileName, submittedData.format)[0]} ar auto-rotate camera-controls ios-src={getEntityFileURL(submittedData.fileName, submittedData.format)[1]} ref={modelViewerRef} />
                 </div>
             </div>
         </div>
